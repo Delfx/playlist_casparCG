1f2de6f2126f66ba9bc0b17ec135bb5807b95066
Lukas Motiejūnas <motiejunas.lukas@gmail.com>
2019-06-05 19:22:21 +0300

    creat class videoFileList to get data from backend to frontend

--- render.js
+++ render.js
@@ -1,20 +1,20 @@
-// const { ipcRenderer } = require('electron');
-//
-//
-//
-//
-// ipcRenderer.send('get-all-available-videos');
-//
-// ipcRenderer.on('all-available-videos', (event, data) => {
-//
-//
-//     const videos = JSON.parse(data);
-//     console.log(videos)
-//     ipcRenderer.send('playout', data);
-//
-//
-// });
-//
-//
-//
-//
+const { ipcRenderer } = require('electron');
+
+
+
+
+ipcRenderer.send('get-all-available-videos');
+
+ipcRenderer.on('all-available-videos', (event, data) => {
+
+
+    const videos = JSON.parse(data);
+    console.log(videos);
+    ipcRenderer.send('playout', data);
+
+
+});
+
+
+
+
